0d9530db08759729f3e4f37ced27a2d09bc66eac
nelk114 <nelk114@disroot.org>
2025-08-03 15:24:38 +0100

    Event loop macro
    For more general X progs
    it'd be probably worth splitting the macro
    to a nonconditional one
    (to facilitate `switch`ing over different types)
    but here this one sufficeþ

--- xcpsd.c
+++ xcpsd.c
@@ -5,8 +5,7 @@
 #include <X11/XKBlib.h>
 #include <X11/keysymdef.h>
 #include <X11/extensions/XTest.h>
-#define NE XNextEvent(d,&e)
-#define itr(b,c) for(b;c;b)
+#define fE(w,i) for(XNextEvent(d,&e);w;XNextEvent(d,&e))if(i)
 #define E(s,e) write(2,s "\n",sizeof(s)),e<0?0:exit(e)
 int main(int C,char**V){
 setlocale(LC_ALL,"");
@@ -29,16 +28,16 @@ KeyCode m=XKeysymToKeycode(d,XK_Multi_key),t;if(!m)E("!!Multi_key",111);{
 	}
 for(int i;i<256;i++){KeySym k;int _;XkbLookupKeySym(d,m,i,&_,&k);if(k==XK_Multi_key)XGrabKey(d,m,i,w,1,GrabModeAsync,GrabModeAsync);}
 XEvent e;XKeyEvent*E=(XKeyEvent*)&e;KeySym k;int S;char _[4];
-if(C>1){write(1,"\n",1);close(1);}itr(NE,)if(e.type==KeyPress){
+if(C>1){write(1,"\n",1);close(1);}fE(,e.type==KeyPress){
 	XmbLookupString(c,E,_,4,&k,&S);XFilterEvent(&e,0);if(k==XK_Multi_key){
 		{Window f;int F;XGetInputFocus(d,&f,&F);XGrabKeyboard(d,w,1,GrabModeAsync,GrabModeAsync,0);XSetInputFocus(d,f,F,0);}
-		itr(NE,)if(e.type==KeyPress){
+		fE(,e.type==KeyPress){
 			if(XmbLookupString(c,E,_,4,&k,&S)&&!XFilterEvent(&e,0)){
 				XUngrabKeyboard(d,0);
 				XChangeKeyboardMapping(d,t,1,&k,1);XSync(d,0);
 				XTestFakeKeyEvent(d,t,1,0);XTestFakeKeyEvent(d,t,0,0);
 				XGrabKeyboard(d,w,1,GrabModeAsync,GrabModeAsync,0);
-				XSelectInput(d,w,KeyReleaseMask);XFlush(d);itr(NE,e.type!=KeyRelease);XSelectInput(d,w,KeyPressMask);XFlush(d);
+				XSelectInput(d,w,KeyReleaseMask);XFlush(d);fE(e.type!=KeyRelease,0);XSelectInput(d,w,KeyPressMask);XFlush(d);
 				XUngrabKeyboard(d,0);
 				k=0;XChangeKeyboardMapping(d,t,1,&k,1);XSync(d,0);
 				break;
