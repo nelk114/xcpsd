bedd878d4b5778de117216abe77d1994fb2dfb02
nelk114 <nelk114@disroot.org>
2025-08-03 15:16:07 +0100

    Terser warning macro
    -1 for only warnings; otherwise exit status

--- xcpsd.c
+++ xcpsd.c
@@ -7,25 +7,25 @@
 #include <X11/extensions/XTest.h>
 #define NE XNextEvent(d,&e)
 #define itr(b,c) for(b;c;b)
-#define W(s) write(2,s "\n",sizeof(s))
+#define E(s,e) write(2,s "\n",sizeof(s)),e<0?0:exit(e)
 int main(int C,char**V){
 setlocale(LC_ALL,"");
-Display*d=XOpenDisplay(getenv("DISPLAY"));if(!d)W("!!$DISPLAY"),exit(111);
-{int _;if(!XTestQueryExtension(d,&_,&_,&_,&_))W("!!XTest"),exit(111);}
-Window w=RootWindow(d,DefaultScreen(d));if(!w)W("!!RootWindow"),exit(111);
+Display*d=XOpenDisplay(getenv("DISPLAY"));if(!d)E("!!$DISPLAY",111);
+{int _;if(!XTestQueryExtension(d,&_,&_,&_,&_))E("!!XTest",111);}
+Window w=RootWindow(d,DefaultScreen(d));if(!w)E("!!RootWindow",111);
 XSelectInput(d,w,KeyPressMask);XFlush(d);
 XIC c=0;{
-	if(!XSetLocaleModifiers("@im=none"))W("!SetLocaleModifiers");/*Unclear this is necessary*/
-	XIM m=XOpenIM(d,0,0,0);XIMStyles*S;XIMStyle s;if(!m)W("!!XIM"),exit(111);
-	if(XGetIMValues(m,XNQueryInputStyle,&S,0)||!S)W("!XIMStyle");if(S){
+	if(!XSetLocaleModifiers("@im=none"))E("!SetLocaleModifiers",-1);/*Unclear this is necessary*/
+	XIM m=XOpenIM(d,0,0,0);XIMStyles*S;XIMStyle s;if(!m)E("!!XIM",111);
+	if(XGetIMValues(m,XNQueryInputStyle,&S,0)||!S)E("!XIMStyle",-1);if(S){
 		s=0;int i=0;for(;i<S->count_styles;i++)if((s=S->supported_styles[i])==(XIMPreeditNothing|XIMStatusNothing))break;
-		if(i==S->count_styles)W("!XIMStyle");XFree(S);
+		if(i==S->count_styles)E("!XIMStyle",-1);XFree(S);
 		}
-	if(m&&s&&!(c=XCreateIC(m,XNInputStyle,s,XNClientWindow,w,XNFocusWindow,w,0)))W("!!XIC"),exit(111);
+	if(m&&s&&!(c=XCreateIC(m,XNInputStyle,s,XNClientWindow,w,XNFocusWindow,w,0)))E("!!XIC",111);
 	}
-KeyCode m=XKeysymToKeycode(d,XK_Multi_key),t;if(!m)W("!!Multi_key"),exit(111);{
+KeyCode m=XKeysymToKeycode(d,XK_Multi_key),t;if(!m)E("!!Multi_key",111);{
 	int M,m,n;XDisplayKeycodes(d,&m,&M);KeySym*K=XGetKeyboardMapping(d,m,M-m+1,&n);
-	int i=m;for(;i<M;i++)if(!K[(i-m)*n]){t=i;break;}XFree(K);if(i==M)W("!!KS"),exit(111);
+	int i=m;for(;i<M;i++)if(!K[(i-m)*n]){t=i;break;}XFree(K);if(i==M)E("!!KS",111);
 	}
 for(int i;i<256;i++){KeySym k;int _;XkbLookupKeySym(d,m,i,&_,&k);if(k==XK_Multi_key)XGrabKey(d,m,i,w,1,GrabModeAsync,GrabModeAsync);}
 XEvent e;XKeyEvent*E=(XKeyEvent*)&e;KeySym k;int S;char _[4];
