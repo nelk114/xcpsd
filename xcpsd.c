beaa7b6568d2795b5f22ecdd0200df3f3423560c
nelk114 <nelk114@disroot.org>
2025-08-23 19:53:47 +0100

    Abuse short‐circuit eval
    The extra brackets and `void,int` pay for themselves it seems

--- xcpsd.c
+++ xcpsd.c
@@ -6,29 +6,29 @@
 #include <X11/keysymdef.h>
 #include <X11/extensions/XTest.h>
 #define fE(w,i) for(;XNextEvent(d,&e),w;)if(i)
-#define E(s,e) write(2,s "\n",sizeof(s)),e<0?0:exit(e)
+#define E(s,e) (write(2,s "\n",sizeof(s)),e<0?0:(exit(e),0))
 int L(XIC c,XEvent*e,KeySym*k,int*S){char _[4];return!!XmbLookupString(c,(XKeyEvent*)e,_,4,k,S)&!XFilterEvent(e,0);}
-int H(Display*d,XErrorEvent*e){if(e->error_code==BadAccess)E("!!Grab",111);else XSetErrorHandler(0);}
+int H(Display*d,XErrorEvent*e){e->error_code==BadAccess&&E("!!Grab",111);XSetErrorHandler(0);}
 int main(int C,char**V){
 setlocale(LC_ALL,"");
-Display*d=XOpenDisplay(0);if(!d)E("!!$DISPLAY",111);
-{int _;if(!XTestQueryExtension(d,&_,&_,&_,&_))E("!!XTest",111);}
-Window w=RootWindow(d,DefaultScreen(d));if(!w)E("!!RootWindow",111);
+Display*d=XOpenDisplay(0);d||E("!!$DISPLAY",111);
+{int _;XTestQueryExtension(d,&_,&_,&_,&_)||E("!!XTest",111);}
+Window w=RootWindow(d,DefaultScreen(d));w||E("!!RootWindow",111);
 XIC c=0;{
-	if(!XSetLocaleModifiers("@im=none"))E("!SetLocaleModifiers",-1);
-	XIM m=XOpenIM(d,0,0,0);XIMStyles*S;XIMStyle s;if(!m)E("!!XIM",111);
-	if(XGetIMValues(m,XNQueryInputStyle,&S,0)||!S)E("!XIMStyle",-1);if(S){
+	XSetLocaleModifiers("@im=none")||E("!SetLocaleModifiers",-1);
+	XIM m=XOpenIM(d,0,0,0);XIMStyles*S;XIMStyle s;m||E("!!XIM",111);
+	!XGetIMValues(m,XNQueryInputStyle,&S,0)&&S||E("!XIMStyle",-1);if(S){
 		s=0;int i=0;for(;i<S->count_styles;i++)if((s=S->supported_styles[i])==(XIMPreeditNothing|XIMStatusNothing))break;
-		if(i==S->count_styles)E("!XIMStyle",-1);XFree(S);
+		i==S->count_styles&&E("!XIMStyle",-1);XFree(S);
 		}
-	if(m&&s&&!(c=XCreateIC(m,XNInputStyle,s,XNClientWindow,w,0)))E("!!XIC",111);
+	s&&!(c=XCreateIC(m,XNInputStyle,s,XNClientWindow,w,0))&&E("!!XIC",111);
 	}
-KeyCode m=XKeysymToKeycode(d,XK_Multi_key),t;if(!m)E("!!Multi_key",111);{
+KeyCode m=XKeysymToKeycode(d,XK_Multi_key),t;m||E("!!Multi_key",111);{
 	int M,m,n;XDisplayKeycodes(d,&m,&M);KeySym*K=XGetKeyboardMapping(d,m,M-m+1,&n);
-	int i=m;for(;i<M;i++)if(!K[(i-m)*n]){t=i;break;}XFree(K);if(i==M)E("!!KS",111);
+	int i=m;for(;i<M;i++)if(!K[(i-m)*n]){t=i;break;}XFree(K);i==M&&E("!!KS",111);
 	}
 XSetErrorHandler(H);XSelectInput(d,w,KeyPressMask);
-for(int i;i<256;i++){KeySym k;int _;XkbLookupKeySym(d,m,i,&_,&k);if(k==XK_Multi_key)XGrabKey(d,m,i,w,1,GrabModeAsync,GrabModeAsync);}
+for(int i;i<256;i++){KeySym k;int _;XkbLookupKeySym(d,m,i,&_,&k);k==XK_Multi_key&&XGrabKey(d,m,i,w,1,GrabModeAsync,GrabModeAsync);}
 XSync(d,0);
 XEvent e;KeySym k;int S;if(C>1)write(1,"\n",1),close(1);fE(1,e.type==KeyPress&&(L(c,&e,&k,&S),k==XK_Multi_key)){
 #define v ((XKeyEvent*)&e)->time
