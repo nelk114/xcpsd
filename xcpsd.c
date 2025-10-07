4b99605dfd73752bf8b0eb5400f51bc0c0716796
nelk114 <nelk114@disroot.org>
2025-10-07 22:48:31 +0100

    More redundant brackets
    Ok technically this does change the structure of the expression,
    but the result is the same:
    return a dummy 0
    and coërce exit(3) to int
    so it can be used with && and ||

--- xcpsd.c
+++ xcpsd.c
@@ -7,7 +7,7 @@
 #include <X11/extensions/XTest.h>
 #define fI(n) for(int i=0;i<n;i++)
 #define fE for(;XNextEvent(d,&e),1;)
-#define E(s,e) (write(2,s "\n",sizeof(s)),e<0?0:(exit(e),0))
+#define E(s,e) (write(2,s "\n",sizeof(s)),e<0?0:exit(e),0)
 int L(XIC c,XEvent*e,KeySym*k,int*S){char _[4];return!!XmbLookupString(c,(XKeyEvent*)e,_,4,k,S)&!XFilterEvent(e,0);}
 int H(Display*d,XErrorEvent*e){e->error_code==BadAccess&&E("!!Grab",111);XSetErrorHandler(0);}
 int main(int C,char**V){
